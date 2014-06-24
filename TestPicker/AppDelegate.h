343aec917f151d66d2427f11f150161024ec5fd6
Musale, Ashish <Ashish_Musale@intuit.com>
2014-06-24 17:28:24 -0400

    Initial Commit

---
+++ TestPicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestPicker
+//
+//  Created by Musale, Ashish on 6/24/14.
+//  Copyright (c) 2014 Ashish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
